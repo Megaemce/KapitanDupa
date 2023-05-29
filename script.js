c494a8d9bb005e4631b9a1239eea632c81fe6208
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-05-29 23:53:27 +0800

    Add sounds. Fix issue with the setInterval not being cancelled

--- script.js
+++ script.js
@@ -1,79 +1,105 @@
-const start = document.getElementById("start");
+// ** CONSTANS ** //
+const topOne = getBestScores(1)[0] ? getBestScores(1)[0].spacebarCount : 0;
+const figures = document.getElementById("figures");
 const inputDiv = document.getElementById("input");
 const gameOver = document.getElementById("gameOver");
-const element1 = document.getElementById("element1");
-const elements = document.getElementById("elements-container");
 const playerImg = document.getElementById("playerImg");
 const nameInput = document.getElementById("nameInput");
-const opponentImg = document.getElementById("opponentImg");
 const scoreValue = document.getElementById("scoreValue");
 const timerValue = document.getElementById("timerValue");
+const startButton = document.getElementById("start");
+const opponentImg = document.getElementById("opponentImg");
 const finalScoreValue = document.getElementById("finalScoreValue");
 const exclamationMark = document.getElementById("!");
 const finalScoreMessage = document.getElementById("finalScore");
 const scoreboardDisplay = document.getElementById("scoreboard");
-
-let gameActive = true;
-let keyDownTime;
+const nicSound = new Audio("nicNieCzuje.mp3");
+const startSound = new Audio("rundaPierwsza.mp3");
+const actionSound = new Audio("dlaczegoNieRypiecie.mp3");
+const koncowkaSound = new Audio("koncowka.mp3");
+const gameOverSound = new Audio("gameOver.mp3");
+const wpiszLoginSound = new Audio("wpiszLogin.mp3");
+const miernyWynikSound = new Audio("miernyWynik.mp3");
+const najwyzszyWynikSound = new Audio("najwyzszyWynik.mp3");
+// ** VARIABLES ** //
+let time = 9;
+let gameActive = false;
 let spacebarCount = 0;
-let isSpacebarHeld = false;
-let keyPressTimeout;
-let countdownTimeout;
 let isSpacebarPressed = false;
 
+// ** FUNCTIONS ** //
+// start the action sound, clock and show the seconds in the timer
 function startCountdown() {
-    let timeLeft = 3;
-    countdownTimeout = setInterval(function () {
-        if (timeLeft >= 0) {
-            timerValue.textContent = timeLeft + "sek";
-            timeLeft--;
+    actionSound.play();
+    const countdownInterval = setInterval(function () {
+        if (time >= 0) {
+            timerValue.textContent = time + "sek";
+            time--;
+            // hacky sound loop
+            actionSound.onended = () => {
+                nicSound.play();
+                nicSound.onended = () => {
+                    actionSound.play();
+                };
+            };
         } else {
-            clearInterval(countdownTimeout);
+            actionSound.pause();
+            clearInterval(countdownInterval);
             endGame();
         }
     }, 1000);
 }
-
+//
 function endGame() {
     gameActive = false;
+    figures.style.display = "none";
     gameOver.style.display = "block";
     gameOver.style.animation = "blink 0.3s 10";
-
-    clearInterval(countdownTimeout);
 
     document.onkeyup = undefined;
     document.onkeydown = undefined;
 
-    setTimeout(() => {
-        gameOver.style.display = "none";
+    gameOverSound.play();
+    gameOverSound.onended = () => {
+        // don't save the score if it's not best than before
+        if (spacebarCount < topOne) {
+            miernyWynikSound.play();
+            miernyWynikSound.onended = () => {
+                koncowkaSound.play();
+            };
+        } else {
+            gameOver.style.display = "none";
+            finalScoreValue.textContent = spacebarCount;
+            finalScoreMessage.style.display = "flex";
+            exclamationMark.style.animation = "blink 0.6s 5";
+            finalScoreValue.style.animation = "blink 0.55s 5";
 
-        finalScoreValue.textContent = spacebarCount;
-        finalScoreMessage.style.display = "flex";
-        exclamationMark.style.animation = "blink 0.6s 5";
-        finalScoreValue.style.animation = "blink 0.55s 5";
+            najwyzszyWynikSound.play();
+            najwyzszyWynikSound.onended = () => {
+                wpiszLoginSound.play();
+                finalScoreMessage.style.display = "none";
+                inputDiv.style.display = "flex";
+                nameInput.style.display = "block";
+                nameInput.focus();
+            };
+        }
+    };
+}
 
-        setTimeout(() => {
-            finalScoreMessage.style.display = "none";
-            inputDiv.style.display = "flex";
-            nameInput.style.display = "block";
-            nameInput.focus();
-        }, 3100);
-    }, 3100);
+// display only the top _number_ scores
+function getBestScores(number) {
+    const savedScores = JSON.parse(localStorage.getItem("scores")) || [];
 
-    elements.style.display = "none";
+    savedScores.sort((a, b) => b.spacebarCount - a.spacebarCount);
+
+    return savedScores.slice(0, number);
 }
 
 function showScoreboard() {
     scoreboardDisplay.innerHTML = "<h3>Scoreboard</h3>";
-    const savedScores = JSON.parse(localStorage.getItem("scores")) || [];
+    const topTen = getBestScores(10);
 
-    // Sort the scores array in descending order
-    savedScores.sort((a, b) => b.spacebarCount - a.spacebarCount);
-
-    // Display only the top 10 scores
-    const topScores = savedScores.slice(0, 10);
-
-    topScores.forEach(function (score, index) {
+    topTen.forEach(function (score, index) {
         scoreboardDisplay.innerHTML +=
             "<p>" +
             (index + 1) +
@@ -84,6 +110,7 @@ function showScoreboard() {
             "</p>";
     });
     scoreboardDisplay.style.display = "flex";
+    koncowkaSound.play();
 }
 
 function saveScore() {
@@ -102,22 +129,13 @@ function saveScore() {
     }
 }
 
-function handleKeyDown() {
-    if (!isSpacebarPressed) {
-        playerImg.setAttribute("src", "active.svg");
-        isSpacebarPressed = true;
-        keyDownTime = new Date().getTime();
-        keyPressTimeout = setTimeout(handleKeyUp, 100);
-    }
-}
-
-function handleKeyUp() {
-    if (isSpacebarPressed && !isSpacebarHeld) {
+// ** HANDLES ** //
+// hanle when user finish pushing the space or finish the touch
+function handleActionFinish() {
+    if (isSpacebarPressed) {
         playerImg.setAttribute("src", "deactive.svg");
-        isSpacebarHeld = true;
     }
     isSpacebarPressed = false;
-    clearTimeout(keyPressTimeout);
     spacebarCount += 10;
 
     const hitsNo = Math.floor(spacebarCount / 100);
@@ -130,62 +148,46 @@ function handleKeyUp() {
     scoreValue.textContent = spacebarCount;
 }
 
-// global hook for the start
-document.onkeydown = () => {
-    start.style.display = "none";
-    elements.style.display = "flex";
-    startCountdown();
+startButton.onclick = () => {
+    startSound.play();
+    startButton.style.animation = "blink 0.9s 10";
+    // start the game when the sound has ended
+    startSound.onended = () => {
+        startButton.style.display = "none";
+        figures.style.display = "flex";
 
-    document.onkeydown = (event) => {
-        if (event.code === "Space" && gameActive) {
-            event.preventDefault();
-            handleKeyDown();
-        }
+        gameActive = true;
+        // add all the hooks for the space and touch
+        document.onkeydown = (event) => {
+            if (event.code === "Space") {
+                event.preventDefault();
+                if (gameActive && !isSpacebarPressed) {
+                    playerImg.setAttribute("src", "active.svg");
+                    isSpacebarPressed = true;
+                }
+            }
+        };
+
+        document.ontouchstart = () => {
+            if (gameActive && !isSpacebarPressed) {
+                playerImg.setAttribute("src", "active.svg");
+                isSpacebarPressed = true;
+            }
+        };
+
+        startCountdown();
     };
-};
-
-start.onclick = () => {
-    start.style.display = "none";
-    elements.style.display = "flex";
-    startCountdown();
 };
 
 document.onkeyup = (event) => {
-    keyPressTimeout && clearTimeout(keyPressTimeout);
-    if (event.code === "Space" && gameActive) {
-        if (isSpacebarPressed) {
-            handleKeyUp();
-        }
-        isSpacebarHeld = false;
-        event.preventDefault();
-    }
-};
-
-nameInput.onkeydown = (event) => {
-    if (event.keyCode === 13) {
-        saveScore();
-    }
-};
-
-// touch events
-document.ontouchstart = () => {
-    start.style.display = "none";
-    elements.style.display = "flex";
-    startCountdown();
-
-    document.ontouchstart = () => {
-        if (gameActive) {
-            handleKeyDown();
-        }
-    };
+    gameActive && event.code === "Space" && handleActionFinish();
 };
 
 document.ontouchend = () => {
-    keyPressTimeout && clearTimeout(keyPressTimeout);
-    if (gameActive) {
-        if (isSpacebarPressed) {
-            handleKeyUp();
-        }
-        isSpacebarHeld = false;
-    }
+    gameActive && handleActionFinish();
+};
+
+// no save button so just handle enter as it
+nameInput.onkeydown = (event) => {
+    event.keyCode === 13 && saveScore();
 };
